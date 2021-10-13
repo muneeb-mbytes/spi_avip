88cb9238b0c2876df5a9ddfa357076f8f3ac5df3
Chetan Kumar <91043972+chetansagabala@users.noreply.github.com>
2021-10-13 15:24:38 +0530

    Update top.sv

--- uvm_tb/env/top.sv
+++ uvm_tb/env/top.sv
@@ -12,27 +12,26 @@
 
     bit reset;
     bit clock;
-
-  //-------------------------------------------------------
-  // Generating Clock
-  //-------------------------------------------------------
-   /* always
-    begin
-      #10 clock=1;
-      #10 clock=0;    
-    end
-*/
+//-------------------------------------------------------
+// Generating Clock
+//-------------------------------------------------------
+ /* always
+  begin
+    #10 clock=1;
+    #10 clock=0;    
+  end
+  */
 //-------------------------------------------------------
 // Here the interface name is spi_i
 //-------------------------------------------------------
 
- //   spi_if in0(clock);
+//   spi_if in0(clock);
 //-------------------------------------------------------
 //Setting interface in database with visibility to all lower components
 //-------------------------------------------------------
     initial 
     begin
-   //   uvm_config_db#(virtual spi_if)::set(null,"*","vif",in0);
-      run_test("base_test");
+//      uvm_config_db#(virtual spi_if)::set(null,"*","vif",in0);
+        run_test("base_test");
     end
   endmodule
