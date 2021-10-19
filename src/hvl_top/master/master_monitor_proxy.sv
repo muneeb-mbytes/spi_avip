f8e93781096f1ab67bd88bea9bf8973992ff45b6
deepikapatil <deepikapatil@mirafra.com>
2021-10-20 02:16:59 +0530

    master monitor proxy updated

--- src/hvl_top/master/master_monitor_proxy.sv
+++ src/hvl_top/master/master_monitor_proxy.sv
@@ -17,24 +17,24 @@ class master_monitor_proxy extends uvm_component;
   `uvm_component_utils(master_monitor_proxy)
   
    //declaring virtual interface
-   //virtual spi_if.MMON_CB vif;
+   virtual spi_if.MMON_CB vif;
    
    //declaring handle for master config class
-//     master_agent_config  m_cfg;
+    master_agent_config  m_cfg;
    
    //declaring analysis port for the monitor port
-   //uvm_analysis_port #(master_xtn)monitor_port;
+   uvm_analysis_port #(master_xtn)monitor_port;
   
   //-------------------------------------------------------
   // Externally defined Tasks and Functions
   //-------------------------------------------------------
   extern function new(string name = "master_monitor_proxy", uvm_component parent = null);
   extern virtual function void build_phase(uvm_phase phase);
-  //extern virtual function void connect_phase(uvm_phase phase);
+  extern virtual function void connect_phase(uvm_phase phase);
   //extern virtual function void end_of_elaboration_phase(uvm_phase phase);
   //extern virtual function void start_of_simulation_phase(uvm_phase phase);
-  //extern virtual task run_phase(uvm_phase phase);
-
+  extern virtual task run_phase(uvm_phase phase);
+ extern task collect_data();
 endclass : master_monitor_proxy
 
 //--------------------------------------------------------------------------------------------
@@ -50,7 +50,7 @@ function master_monitor_proxy::new(string name = "master_monitor_proxy",
   
   //creating monitor port
   
-//  monitor_port=new("monitor_port",this);
+  monitor_port=new("monitor_port",this);
 endfunction : new
 
 //--------------------------------------------------------------------------------------------
@@ -63,10 +63,10 @@ endfunction : new
 function void master_monitor_proxy::build_phase(uvm_phase phase);
   super.build_phase(phase);
 
- /*       if(!uvm_config_db #(master_agent_config)::get(this,"","master_agent_config",m_cfg))
+        if(!uvm_config_db #(master_agent_config)::get(this,"","master_agent_config",m_cfg))
         begin
         `uvm_fatal("TB CONFIG","cannot get() m_cfg from uvm_config");
-        end */
+        end 
 endfunction : build_phase
 
 //--------------------------------------------------------------------------------------------
@@ -76,31 +76,9 @@ endfunction : build_phase
 // Parameters:
 //  phase - uvm phase
 //--------------------------------------------------------------------------------------------
-/*function void master_monitor_proxy::connect_phase(uvm_phase phase);
+function void master_monitor_proxy::connect_phase(uvm_phase phase);
       vif = m_cfg.vif;
 endfunction : connect_phase
-
-//--------------------------------------------------------------------------------------------
-// Function: end_of_elaboration_phase
-// <Description_here>
-//
-// Parameters:
-//  phase - uvm phase
-//--------------------------------------------------------------------------------------------
-function void master_monitor_proxy::end_of_elaboration_phase(uvm_phase phase);
-  super.end_of_elaboration_phase(phase);
-endfunction  : end_of_elaboration_phase
-
-//--------------------------------------------------------------------------------------------
-// Function: start_of_simulation_phase
-// <Description_here>
-//
-// Parameters:
-//  phase - uvm phase
-//--------------------------------------------------------------------------------------------
-function void master_monitor_proxy::start_of_simulation_phase(uvm_phase phase);
-  super.start_of_simulation_phase(phase);
-endfunction : start_of_simulation_phase
 
 //--------------------------------------------------------------------------------------------
 // Task: run_phase
@@ -110,17 +88,35 @@ endfunction : start_of_simulation_phase
 //  phase - uvm phase
 //--------------------------------------------------------------------------------------------
 task master_monitor_proxy::run_phase(uvm_phase phase);
+forever
+begin
+  collect_data();
+end
+endtask :run_phase
 
-  phase.raise_objection(this, "master_monitor_proxy");
+//--------------------------------------------------------------------------------------------
+// task collect data
+// creating a handle for master transaction
+//--------------------------------------------------------------------------------------------
+task master_monitor_proxy::collect_data();
+  master_xtn data_sent;
+  data_sent = master_xtn::type_id::create("data_sent");
+ 
+  fork
+  begin
+    @(vif.mmon_cb);
+    data_sent.sclk=vif.mmon_cb.sclk;
+    data_sent.ss=vif.mmom_cb.ss;
+    data_sent.mosi0=vif.mmon_cb.mosi0;
+    data_sent.miso0=vif.mmon_cb.miso0;
+  end
+join
+monior_por.write(data_sent);
 
-  super.run_phase(phase);
+'uvm_info("MASTER_MONITOR_PROXY",$sformatf("printing from monitor \n %s", data_sent)
+.sprint()),UVM_LOW)
 
-  // Work here
-  // ...
+endtask:collect_data
 
-  phase.drop_objection(this);
-
-endtask : run_phase
-*/
 `endif
 
