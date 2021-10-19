4a1affce116b0cfab6bc001ac9de7289ff623d65
MukulTomar1 <mukultomar@mirafra.com>
2021-10-20 02:21:52 +0530

    master_xtn_updated

--- src/hvl_top/master/master_xtn.sv
+++ src/hvl_top/master/master_xtn.sv
@@ -12,6 +12,17 @@
   //register with factory so we can override with uvm method in future if necessary.
 
   `uvm_object_utils(master_xtn)
+ bit sclk;
+ bit ss_n;
+
+ rand bit [1:0]cpol;
+ rand bit [1:0]cpha;
+
+ bit [7:0]data_in_mosi;
+
+ rand bit mosi;
+
+ bit miso; 
 
   //-------------------------------------------------------
   // Externally defined Tasks and Functions
@@ -33,6 +44,16 @@
 
   function void master_xtn::do_print(uvm_printer printer);
     super.do_print(printer);
+ 
+    printer.print_field("sclk", sclk, 1,UVM_DEC);
+    printer.print_field("ss_n", ss_n, 1,UVM_DEC);
+    printer.print_field("cpol", cpol, 2, UVM_DEC);
+    printer.print_field("cpha", cpha, 2, UVM_DEC);
+  
+    printer.print_field("data_in_mosi", data_in_mosi, 8, UVM_HEX);
+    printer.print_field("miso", miso, 1, UVM_DEC);
+    printer.print_field("mosi", mosi, 1, UVM_DEC);
+  
   endfunction : do_print
   
 
