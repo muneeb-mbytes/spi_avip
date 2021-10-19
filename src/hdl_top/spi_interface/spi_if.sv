cf408cfc36f3614717e2ddd42347fa2b8f32ef5f
sahanapalesh <sahanapalesh@mirafra.com>
2021-10-20 00:52:29 +0530

    adding interface

--- src/hdl_top/spi_interface/spi_if.sv
+++ src/hdl_top/spi_interface/spi_if.sv
@@ -2,64 +2,125 @@
 // Module       : Interface
 // Description  : Declaration of pin level signals as logic
 //--------------------------------------------------------------------------------------------
-interface spi_if();
+interface spi_if;
 
-  logic clk;
-  logic rst;
   logic sclk;
-  logic cs;
-   
-  //master out slave in
-  logic mosi;
+  logic ss;
+  
+  //-------------------------------------------------------
+  // Master out Slave in
+  //-------------------------------------------------------
+  logic mosi0;
   logic mosi1;
   logic mosi2;
-  logic mosi3;  
-             
-  //master in slave out
-  logic miso;
+  logic mosi3;
+  //-------------------------------------------------------
+  // Master out Slave in
+  //-------------------------------------------------------
+  logic miso0;
   logic miso1;
   logic miso2;
   logic miso3;
 
 //-------------------------------------------------------
-// Slave Interface
+// Clocking block for slave driver bfm
 //-------------------------------------------------------
-
-//-------------------------------------------------------
-// Clocking block for slave driver
-//-------------------------------------------------------
-clocking s_drv_cb @(posedge sclk);
+clocking sd_cb @(posedge sclk);
+  //-------------------------------------------------------
+  // Input and output skews
+  //-------------------------------------------------------
+  default input #1 output #2;
   
-  //input and output skews
-  default input #1 output #0;
-  //signals from slave to master  
-  input cs;
-  output miso;
+  //-------------------------------------------------------
+  // Signals from slave to master  
+  //-------------------------------------------------------
+  input ss;
+  input mosi0;
+  input mosi1;
+  input mosi2;
+  input mosi3;
+  output miso0;
   output miso1;
   output miso2;
   output miso3;
-
-endclocking : s_drv_cb
+endclocking : sd_cb
 
 //-------------------------------------------------------
-// Clocking block for slave monitor
+// Clocking block for slave driver proxy
 //-------------------------------------------------------
-clocking s_mon_cb @(posedge sclk);
+clocking sdp_cb @(posedge sclk);
+  //-------------------------------------------------------
+  // Input and output skews
+  //-------------------------------------------------------
+  default input #1 output #2;
   
-  //input and output skews  
-  default input #1 output #0 ;
-  //signals from slave to  master
-  input miso;
+  //-------------------------------------------------------
+  // Signals from slave to master  
+  //-------------------------------------------------------
+  input ss;
+  input mosi0;
+  input mosi1;
+  input mosi2;
+  input mosi3;
+  output miso0;
+  output miso1;
+  output miso2;
+  output miso3;
+endclocking : sdp_cb
+
+//-------------------------------------------------------
+// Clocking block for slave monitor bfm
+//-------------------------------------------------------
+clocking sm_cb @(posedge sclk);
+  //-------------------------------------------------------
+  // Input and output skews
+  //-------------------------------------------------------
+  default input #1 output #2;
+  
+  //-------------------------------------------------------
+  // Signals from slave to master  
+  //-------------------------------------------------------
+  input ss;
+  input mosi0;
+  input mosi1;
+  input mosi2;
+  input mosi3;
+  input miso0;
   input miso1;
   input miso2;
   input miso3;
+endclocking : sm_cb
 
-endclocking : s_mon_cb
+//-------------------------------------------------------
+// Clocking block for slave monitor proxy
+//-------------------------------------------------------
+clocking smp_cb @(posedge sclk);
+  //-------------------------------------------------------
+  // Input and output skews
+  //-------------------------------------------------------
+  default input #1 output #2;
+  
+  //-------------------------------------------------------
+  // Signals from slave to master  
+  //-------------------------------------------------------
+  input ss;
+  input mosi0;
+  input mosi1;
+  input mosi2;
+  input mosi3;
+  input miso0;
+  input miso1;
+  input miso2;
+  input miso3;
+endclocking : smp_cb
 
 //-------------------------------------------------------
 // Declaring modports for slave driver and monitor
 //-------------------------------------------------------
-  modport S_DRV_CB (output miso, input sclk, cs, mosi);
-  modport S_MON_CB (input sclk, cs, mosi, miso);
+  modport SD_MP (clocking sd_cb);
+  modport SDP_MP (clocking sdp_cb);
+  modport SM_MP (clocking sm_cb);
+  modport SMP_MP (clocking smp_cb);
 
-endinterface : spi_if
+endinterface: spi_if 
+
