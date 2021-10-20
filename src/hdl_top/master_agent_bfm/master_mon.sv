1ad711d9c78f1927f07ec81462f4ce5b00d4be04
MukulTomar1 <mukultomar@mirafra.com>
2021-10-21 04:24:45 +0530

    logic for the master monitor bfm

--- src/hdl_top/master_agent_bfm/master_mon.sv
+++ src/hdl_top/master_agent_bfm/master_mon.sv
@@ -1,11 +1,50 @@
 //-------------------------------------------------------
-//master monitor
+//Module : Master Monitor BFM
+//
+//Description : Connection is from the master monitor bfm to the monitor proxy 
 //-------------------------------------------------------
-module master_mon(spi_intf);
+module master_monitor_bfm (spi_if.MON_MP intf);
 
-  initial
-  begin
-    $display("Master Monitor BFM");
-  end
+//--------------------------------------------------------------------------------------------
+// Creating the handel for the proxy monitor
+//--------------------------------------------------------------------------------------------
+    import spi_master_pkg::master_monitor_proxy;
+    master_monitor_proxy m_mon_proxy  
+ 
+//
+//    bit [2:0]cpol;
+//    bit [2:0]cphs;
+//
+//
+//    //task 1
+//    task write(bit [7:0]mosi);
+//    
+//    if(!cs) begin
+//      case(cpol,cphase);
+//      
+//      2'b00 : begin
+//              data.mosi0 <= intf.mosi_pos_cb.mosi0;
+//              data.miso0 <= intf.miso_pos_cb.miso0;
+//              end
+//
+//      2'b01 : begin
+//              data.mosi1 <= intf.mosi_pos_cb.mosi1;
+//              data.miso1 <= intf.miso_neg_cb.miso1;
+//              end
+//
+//      2'b10 : begin
+//              data.mosi1 <= intf.mosi_neg_cb.mosi2;
+//              data.miso1 <= intf.miso_pos_cb.miso2;
+//              end
+//
+//      2'b11 : begin
+//              data.mosi1 <= intf.mosi_neg_cb.mosi3;
+//              data.miso1 <= intf.miso_neg_cb.miso3;
+//              end
 
-endmodule
+ //--------------------------------------------------------------------------------------------
+ // 2nd part
+ //--------------------------------------------------------------------------------------------
+      
+    //task 1
+    task po0_ph0(bit [7:0]mosi);
