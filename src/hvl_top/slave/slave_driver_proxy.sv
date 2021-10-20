777f373ef860f17aec1f279df3e9d2beecb92b35
sivareddy_mbytes <sivareddykostam@mirafra.com>
2021-10-21 01:40:55 +0530

    written a logic for slave_driver_proxy and slave_sequence

--- src/hvl_top/slave/slave_driver_proxy.sv
+++ src/hvl_top/slave/slave_driver_proxy.sv
@@ -10,24 +10,30 @@ class slave_driver_proxy extends uvm_driver#(slave_tx);
   `uvm_component_utils(slave_driver_proxy)
 
   //-------------------------------------------------------
+  // Creating the handle for driver bfm
+  //-------------------------------------------------------
+  virtual slave_driver_bfm v_drv_bfm;
+  
+  //-------------------------------------------------------
   // Externally defined Tasks and Functions
   //-------------------------------------------------------
   extern function new(string name = "slave_driver_proxy", uvm_component parent = null);
   extern virtual function void build_phase(uvm_phase phase);
+  // extern virtual function void connect_phase(uvm_phase phase);
   extern virtual task run_phase(uvm_phase phase);
-
+  
 endclass : slave_driver_proxy
 
 //--------------------------------------------------------------------------------------------
 // Construct: new
 // // TODO(mshariff): add comments
 // Parameters:
-//  name - slave_driver_proxy
-//  parent - parent under which this component is created
+// name - slave_driver_proxy
+// parent - parent under which this component is created
 //--------------------------------------------------------------------------------------------
-function slave_driver_proxy::new(string name = "slave_driver_proxy",
-                                 uvm_component parent = null);
+function slave_driver_proxy::new(string name = "slave_driver_proxy", uvm_component parent = null);
   super.new(name, parent);
+  v_drv_bfm.drv_proxy = this;
 endfunction : new
 
 //--------------------------------------------------------------------------------------------
@@ -36,25 +42,68 @@ endfunction : new
 // <Description_here>
 //
 // Parameters:
-//  phase - uvm phase
+// phase - uvm phase
 //--------------------------------------------------------------------------------------------
 function void slave_driver_proxy::build_phase(uvm_phase phase);
   super.build_phase(phase);
-  // TODO(mshariff): get the interface handle
+  // if(!uvm_config_db #(slave_agent_config)::get(this,"","sa_cfg_h",sa_cfg_h))
+  // `uvm_fatal("CONFIG","cannot get() sa_cfg_h")
+  
+  // if(!uvm_config_db #(slave_driver_bfm)::get(this,"","v_drv_bfm",v_drv_bfm))
+  // `uvm_fatal("CONFIG","cannot get() v_drv_bfm")
 endfunction : build_phase
+
+
+/*
+//--------------------------------------------------------------------------------------------
+// Function: connect_phase
+// Connects driver_proxy and driver_bfm
+//
+// Parameters:
+// phase - stores the current phase
+//--------------------------------------------------------------------------------------------
+function void slave_driver_proxy::connect_phase(uvm_phase phase);
+super.connect_phase(phase);
+// v_drv_bfm = sa_cfg_h.v_drv_bfm;
+
+endfunction : connect_phase
+*/
 
 //--------------------------------------------------------------------------------------------
 // Task: run_phase
-// // TODO(mshariff): 
+// // TODO(mshariff):
 //--------------------------------------------------------------------------------------------
 task slave_driver_proxy::run_phase(uvm_phase phase);
-  `uvm_info(get_type_name(), $sformatf("Inside the slave_driver_proxy"), UVM_LOW)
 
-  // Get the next item
-  // req.print();
+  // slave_tx tx = new();
+  // repeat(2) begin
+  // tx.randomize();
+  // v_drv_bfm.drive_mosi_pos_miso_neg();
+  // tx.print();
+  // end  
+  
+  forever begin
+  seq_item_port.get_next_item(req);
+  v_drv_bfm.mosi_pos_miso_neg (req);
+  seq_item_port.item_done();
+  end
 
-  // Drive to the DUT
+endtask : run_phase
 
-endtask : run_phase 
 
+
+  /*
+  task slave_driver_proxy::over_all_task;
+  repeat(2) begin
+  if (cs == 0) begin
+  case: {cpol,cpha}
+  2'b00: v_drv_bfm.drive_mosi_pos_miso_neg();
+  2'b01: v_drv_bfm.drive_mosi_neg_miso_pos();
+  2'b10: v_drv_bfm.drive_mosi_pos_miso_neg();
+  2'b11: v_drv_bfm.drive_mosi_neg_miso_pos();
+  endcase
+  end
+  end
+  */
+  
 `endif
--- src/hvl_top/slave/slave_sequence.sv
+++ src/hvl_top/slave/slave_sequence.sv
@@ -3,29 +3,25 @@
 
 //--------------------------------------------------------------------------------------------
 // Class: slave_sequence
-// Class Description:
-// slave_sequence is extended from uvm_sequence to create sequence items
+// Slave_sequence is extended from uvm_sequence to create sequence items
 //--------------------------------------------------------------------------------------------
 class slave_sequence extends uvm_sequence #(slave_tx);
-
-  //-------------------------------------------------------
-  // Factory Registration is done to override the object
-  //-------------------------------------------------------
   `uvm_object_utils(slave_sequence)
 
-  //-------------------------------------------------------
-  // Externally defined Tasks and Functions
-  //-------------------------------------------------------
-  extern function new(string name = "slave_sequence");
+//-------------------------------------------------------
+// Externally defined Tasks and Functions
+//-------------------------------------------------------
+extern function new(string name = "slave_sequence");
 
 endclass : slave_sequence
 
+
 //--------------------------------------------------------------------------------------------
 // Construct: new
-//  Initializes new memory for slave_sequence
+// Initializes new memory for slave_sequence
 //
 // Parameters:
-//  name - slave_sequence
+// name - slave_sequence
 //--------------------------------------------------------------------------------------------
 function slave_sequence::new(string name = "slave_sequence");
   super.new(name);
@@ -34,28 +30,23 @@ endfunction : new
 //--------------------------------------------------------------------------------------------
 // Class: seq extended from base class
 //--------------------------------------------------------------------------------------------
-
 class s_seq_1 extends slave_sequence;
-
-  //-------------------------------------------------------
-  // Factory Registration is done to override the object
-  //-------------------------------------------------------
   `uvm_object_utils(s_seq_1)
 
-  //-------------------------------------------------------
-  // Externally defined tasks and functions
-  //-------------------------------------------------------
-  extern function new(string name = "s_seq_1");
-  extern virtual task body();
+//-------------------------------------------------------
+// Externally defined tasks and functions
+//-------------------------------------------------------
+extern function new(string name = "s_seq_1");
+extern virtual task body();
 
 endclass : s_seq_1
 
 //--------------------------------------------------------------------------------------------
 // Construct: new
-// Initializes sseq1 class object 
+// Initializes sseq1 class object
 //
 // Parameters:
-//  name - sseq1
+// name - sseq1
 //--------------------------------------------------------------------------------------------
 function s_seq_1::new(string name = "s_seq_1");
   super.new(name);
@@ -63,12 +54,18 @@ endfunction : new
 
 //-------------------------------------------------------
 // task:body
-//  To create request which is comes from driver
+// To create request which is comes from driver
 //-------------------------------------------------------
 
 task s_seq_1::body();
+  repeat(1) begin
+    req = slave_tx::type_id::create("req");
+    start_item(req);
+    if(req.randomize () with {cpol==0; cpha==0; data_master_in_slave_out == 8'b10010010;});
+    `uvm_info("slave_sequence",$sformatf("printing from sequence %s", req.sprint()),UVM_LOW)
+    finish_item(req);
+  end
 
 endtask : body
 
 `endif
-
