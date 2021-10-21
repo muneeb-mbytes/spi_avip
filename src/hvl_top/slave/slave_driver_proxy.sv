6f639cb22db1e090258cb7dc57d7abf0743fd050
sivareddy_mbytes <sivareddykostam@mirafra.com>
2021-10-22 04:44:38 +0530

    written the Tasks in slave_driver_proxy

--- src/hvl_top/slave/slave_driver_proxy.sv
+++ src/hvl_top/slave/slave_driver_proxy.sv
@@ -9,135 +9,106 @@
 class slave_driver_proxy extends uvm_driver#(slave_tx);
   `uvm_component_utils(slave_driver_proxy)
 
+  slave_tx tx;
+
   //-------------------------------------------------------
   // Creating the handle for driver bfm
   //-------------------------------------------------------
-  virtual slave_driver_bfm v_drv_bfm;
+  virtual slave_driver_bfm s_drv_bfm_h;
 
   //-------------------------------------------------------
   // Externally defined Tasks and Functions
   //-------------------------------------------------------
   extern function new(string name = "slave_driver_proxy", uvm_component parent = null);
   extern virtual function void build_phase(uvm_phase phase);
-
-//  extern virtual function void connect_phase(uvm_phase phase);
-
+  extern virtual function void connect_phase(uvm_phase phase);
   extern virtual task run_phase(uvm_phase phase);
-  
+  extern virtual task drive_to_dut();;
+
 endclass : slave_driver_proxy
 
 //--------------------------------------------------------------------------------------------
 // Construct: new
-// Description: It's creates the components and objects
+// Description:It creates the memory for components and objects 
 // Parameters:
-// name - slave_driver_proxy
-// parent - parent under which this component is created
+//  name - slave_driver_proxy
+//  parent - parent under which this component is created
 //--------------------------------------------------------------------------------------------
 function slave_driver_proxy::new(string name = "slave_driver_proxy", uvm_component parent = null);
   super.new(name, parent);
-  v_drv_bfm.drv_proxy = this;
-endfunction : new
-
+  s_drv_bfm_h.s_drv_proxy_h = this;
+endfunctio: slave_driver_proxy
 //--------------------------------------------------------------------------------------------
 // Function: build_phase
-// 
-//Description: It will create memory for required classes
+// Description: in build_phase we are getting the slave_agent_config and slave_driver_bfm
+// interface
 //
 // Parameters:
-// phase - uvm phase
+//  phase - uvm phase
 //--------------------------------------------------------------------------------------------
 function void slave_driver_proxy::build_phase(uvm_phase phase);
   super.build_phase(phase);
-
-//  if(!uvm_config_db #(slave_agent_config)::get(this,"","sa_cfg_h",sa_cfg_h))
+//  if(!uvm_config_db #(slave_agent_config)::get(this,"","slave_agent_config",sa_cfg_h))
 //		`uvm_fatal("CONFIG","cannot get() sa_cfg_h")
     
-//  if(!uvm_config_db #(slave_driver_bfm)::get(this,"","v_drv_bfm",v_drv_bfm))
-//  	`uvm_fatal("CONFIG","cannot get() v_drv_bfm")
+  if(!uvm_config_db #(slave_driver_bfm)::get(this,"","slave_driver_bfm",s_drv_bfm_h))
+  	`uvm_fatal("CONFIG","cannot get() s_drv_bfm_h")
 endfunction : build_phase
 
 
-/*
 //--------------------------------------------------------------------------------------------
 // Function: connect_phase
-// Connects driver_proxy and driver_bfm
+// Description: Connects driver_proxy and driver_bfm
 //
 // Parameters:
-
 //  phase - stores the current phase
 //--------------------------------------------------------------------------------------------
 function void slave_driver_proxy::connect_phase(uvm_phase phase);
   super.connect_phase(phase);
- // v_drv_bfm = sa_cfg_h.v_drv_bfm;
-
+//  s_drv_bfm_h = sa_cfg_h.s_drv_bfm_h;
 endfunction : connect_phase
-*/
+
 
 //--------------------------------------------------------------------------------------------
 // Task: run_phase
-// Description:In this phase we are getting the sequence_item from sequencer
+// Description: we are getting the transaction(sequence_item) from sequencer  to driver and send
+// it to DUT
+ 
 //--------------------------------------------------------------------------------------------
 task slave_driver_proxy::run_phase(uvm_phase phase);
 
-
-//  slave_tx tx = new();
-//  repeat(2) begin
-//    tx.randomize();
-//    v_drv_bfm.drive_mosi_pos_miso_neg();
-//    tx.print();
-//  end
+//  bit cpol;
+//  bit cpha;
 
   forever begin
     seq_item_port.get_next_item(req);
-    
+  
     drive_to_dut();
-        
+
     seq_item_port.item_done();
   end
-  
-  /*
-  task slave_driver_proxy::over_all_task;
-  repeat(2) begin
-  if (cs == 0) begin
-  case: {cpol,cpha}
-  2'b00: v_drv_bfm.drive_mosi_pos_miso_neg();
-  2'b01: v_drv_bfm.drive_mosi_neg_miso_pos();
-  2'b10: v_drv_bfm.drive_mosi_pos_miso_neg();
-  2'b11: v_drv_bfm.drive_mosi_neg_miso_pos();
-  endcase
+
+endtask : run_phase 
+
+task slave_driver_proxy::drive_to_dut();
+  foreach(req.data_master_in_slave_out[i]) begin
+  //repeat(1) begin
+    bit [7:0] data;
+
+//    bit cpol;
+//    bit cpha;
+    
+   data = req.master_out_slave_in[i];
+   //data = 8'd25;
+
+    case ({tx.cpol,tx.cpha})
+      2'b00: s_drv_bfm_h.drive_mosi_pos_miso_neg(data);
+      2'b01: s_drv_bfm_h.drive_mosi_neg_miso_pos(data);
+      2'b10: s_drv_bfm_h.drive_mosi_pos_miso_neg(data);
+      2'b11: s_drv_bfm_h.drive_mosi_neg_miso_pos(data);
+    endcase
   end
-  end
-  */
-  
-task drive_to_dut();
-  foreach(req.master_out_slave_in[i]) begin
-    bit[7:0] data;
-    
-    data = req.master_out_slave_in[i];
-    
-      case: {cpol,cpha}
-        2'b00: v_drv_bfm.drive_mosi_pos_miso_neg(data);
-        2'b01: v_drv_bfm.drive_mosi_neg_miso_pos(data);
-        2'b10: v_drv_bfm.drive_mosi_pos_miso_neg(data);
-        2'b11: v_drv_bfm.drive_mosi_neg_miso_pos(data);
-      endcase
-    end
-    
-  end
-endtask
-    
-/*
-task slave_driver_proxy::over_all_task;
-  repeat(2) begin
-    if (cs == 0) begin
-      case: {cpol,cpha}
-        2'b00: v_drv_bfm.drive_mosi_pos_miso_neg();
-        2'b01: v_drv_bfm.drive_mosi_neg_miso_pos();
-        2'b10: v_drv_bfm.drive_mosi_pos_miso_neg();
-        2'b11: v_drv_bfm.drive_mosi_neg_miso_pos();
-      endcase
-    end
-end
-*/
-    
+endtask : drive_to_dut
+
 `endif
+
