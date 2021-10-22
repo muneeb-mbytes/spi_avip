b53a499ff444b65afa6881fef46102406abec542
Sahana P <91043932+sahanapalesh@users.noreply.github.com>
2021-10-22 14:48:19 +0530

    Sahana made changes

--- src/hvl_top/hvl_top.sv
+++ src/hvl_top/hvl_top.sv
@@ -13,7 +13,6 @@ module hvl_top;
   // Declaring SPI and Slave_driver_bfm Interface
   //-------------------------------------------------------
   spi_if vif();
-  slave_driver_bfm v_drv_bfm(vif.SLV_DRV_MP);
 
   //-------------------------------------------------------
   // run_test for simulation
@@ -24,10 +23,6 @@ module hvl_top;
     //-------------------------------------------------------
     uvm_config_db #(virtual spi_if)::set(null,"*","spi_if",vif); 
     
-    //-------------------------------------------------------
-    // Setting Slave_driver_bfm Interface
-    //-------------------------------------------------------
-    uvm_config_db #(virtual slave_driver_bfm)::set(null,"*","slave_driver_bfm",v_drv_bfm); 
 
     run_test("base_test");
   end
