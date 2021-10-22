57bb21e82895ece39eb16d1aeb1b81d679d2a8e7
deepikapatil <deepikapatil@mirafra.com>
2021-10-22 08:37:43 +0530

    master monitor proxy updated

--- src/hvl_top/master/master_monitor_proxy.sv
+++ src/hvl_top/master/master_monitor_proxy.sv
@@ -2,42 +2,42 @@
 `define MASTER_MONITOR_PROXY_INCLUDED_
 
 //--------------------------------------------------------------------------------------------
-// Class: master_monitor_proxy
-// <Description of the class
-//Monitor is written by extending uvm_monitor,uvm_monitor is inherited from uvm_component, 
-//A monitor is a passive entity that samples the DUT signals through virtual interface and 
-//converts the signal level activity to transaction level,monitor samples DUT signals but does not drive them.
-//Monitor should have analysis port (TLM port) and virtual interface handle that points to DUT signal
+//Class: master_monitor_proxy
+//Class: master_monitor_proxy
+//This is the HVL master monitor proxy
+//It gets the sampled data from the HDL master monitor and 
+//converts them into transaction items
 //--------------------------------------------------------------------------------------------
 class master_monitor_proxy extends uvm_component;
-  
-  //register with factory so can use create uvm_method and
-  //override in future if necessary
+
+  //Parameter : Data length
+  //Data length of Data_MOSI
+  parameter DATA_LENGTH = 8;
   
   `uvm_component_utils(master_monitor_proxy)
   
-  // Variable: m_cfg
-  // Declaring handle for master agent config class 
-  master_agent_config m_cfg;
 
-   //declaring virtual interface
-   //virtual spi_if.MMON_CB vif;
-   
-   //declaring handle for master config class
-//     master_agent_config  m_cfg;
-   
-   //declaring analysis port for the monitor port
-   //uvm_analysis_port #(master_tx)monitor_port;
+   //Declaring Monitor Analysis Import
+   //uvm_analysis_port #(master_txn) ap;
+   // 
+   //Declaring Virtual Monitor BFM Handle
+   virtual master_monitor_bfm m_mon_bfm_h;
+
+   //Signal : MISO Data-Input
+   bit [DATA_LENGTH-1:0]data_miso;
+
+    //Queue : data_miso_q
+    //Sets of Data_miso data
+    bit [DATA_LENGTH-1:0]data_miso_q[$];   
   
-  //-------------------------------------------------------
-  // Externally defined Tasks and Functions
-  //-------------------------------------------------------
+//------------------------------------------------------
+// Externally defined Tasks and Functions
+//------------------------------------------------------
   extern function new(string name = "master_monitor_proxy", uvm_component parent = null);
   extern virtual function void build_phase(uvm_phase phase);
-  //extern virtual function void connect_phase(uvm_phase phase);
-  //extern virtual function void end_of_elaboration_phase(uvm_phase phase);
-  //extern virtual function void start_of_simulation_phase(uvm_phase phase);
-  //extern virtual task run_phase(uvm_phase phase);
+  extern virtual task run_phase(uvm_phase phase);
+  extern virtual task read_from_mon_bfm(bit CPOL,bit CPHA,bit miso);
+  extern virtual task write(bit [DATA_LENGTH-1:0]data);
 
 endclass : master_monitor_proxy
 
@@ -51,10 +51,8 @@ endclass : master_monitor_proxy
 function master_monitor_proxy::new(string name = "master_monitor_proxy",
                                  uvm_component parent = null);
   super.new(name, parent);
+  //ap = new("ap",this);  
   
-  //creating monitor port
-  
-//  monitor_port=new("monitor_port",this);
 endfunction : new
 
 //--------------------------------------------------------------------------------------------
@@ -67,64 +65,119 @@ endfunction : new
 function void master_monitor_proxy::build_phase(uvm_phase phase);
   super.build_phase(phase);
 
- /*       if(!uvm_config_db #(master_agent_config)::get(this,"","master_agent_config",m_cfg))
+  if(!uvm_config_db #(master_monitor_bfm)::get(this,",","m_mon_bfm_h",m_mon_bfm_h))
         begin
-        `uvm_fatal("TB CONFIG","cannot get() m_cfg from uvm_config");
-        end */
+    `uvm_fatal("FATAL_SMP_MON_BFM","couldn't get m_mon_bfm in master_monitor_proxy"));
+        end 
 endfunction : build_phase
 
 //--------------------------------------------------------------------------------------------
-// Function: connect_phase
-// <Description_here>
-//
-// Parameters:
-//  phase - uvm phase
-//--------------------------------------------------------------------------------------------
-/*function void master_monitor_proxy::connect_phase(uvm_phase phase);
-      vif = m_cfg.vif;
-endfunction : connect_phase
-
-//--------------------------------------------------------------------------------------------
-// Function: end_of_elaboration_phase
-// <Description_here>
-//
-// Parameters:
-//  phase - uvm phase
-//--------------------------------------------------------------------------------------------
-function void master_monitor_proxy::end_of_elaboration_phase(uvm_phase phase);
-  super.end_of_elaboration_phase(phase);
-endfunction  : end_of_elaboration_phase
-
-//--------------------------------------------------------------------------------------------
-// Function: start_of_simulation_phase
-// <Description_here>
-//
-// Parameters:
-//  phase - uvm phase
-//--------------------------------------------------------------------------------------------
-function void master_monitor_proxy::start_of_simulation_phase(uvm_phase phase);
-  super.start_of_simulation_phase(phase);
-endfunction : start_of_simulation_phase
-
-//--------------------------------------------------------------------------------------------
 // Task: run_phase
-// <Description_here>
+// calls tasks defined in master_monitor_bfm
 //
-// Parameters:
-//  phase - uvm phase
 //--------------------------------------------------------------------------------------------
 task master_monitor_proxy::run_phase(uvm_phase phase);
+ `uvm_info(get_type_name(), $sformatf("Inside the master_monitor_proxy"), UVM_LOW)
 
-  phase.raise_objection(this, "master_monitor_proxy");
+  //Will be using this when transaction object in connected
+  //forever begin 
+  //end
+ repeat(1) begin
+       
+    //Variable : CPOL
+    //Clock Polarity 
+    bit CPOL=0;
+       
+    //Signal : CPHA
+    //Clock Phase
+     bit CPHA=0;
 
-  super.run_phase(phase);
+    //Signal : Miso
+    //Master-out Slave-in
+    //bit mosi;
 
-  // Work here
-  // ...
+    //Signal : Miso
+    //Master-in Slave-out
+    bit miso;
+         
+    //Signal : CS
+    //Chip Select
+    //bit cs;
 
-  phase.drop_objection(this);
-
+//-------------------------------------------------------
+//Calling the tasks from monitor bfm
+//------------------------------------------------------
+     read_from_mon_bfm(CPOL,CPHA,miso);    
+   end
 endtask : run_phase
-*/
+
+//-------------------------------------------------------
+//Task : read_from_mon_bfm
+//Used to call the tasks from moitor bfm
+//-------------------------------------------------------
+task master_monitor_proxy::read_from_mon_bfm(bit CPOL,bit CPHA,bit mosi);
+   case({CPOL,CPHA})
+     2'b00 : begin
+                 
+               m_mon_bfm_h.sample_miso_pos_00(miso);
+               $display("data_miso=%d",m_mon_bfm_h.data_miso);
+               write(m_mon_bfm_h.data_miso);
+              end
+
+      2'b01 : begin 
+                
+               m_mon_bfm_h.sample_miso_neg_01(miso);
+               write(m_mon_bfm_h.data_miso);                          end
+
+      2'b10 : begin                  
+  
+               m_mon_bfm_h.sample_miso_pos_10(miso);
+               write(m_mon_bfm_h.data_miso);
+             end
+                                                              2'b11 : begin
+                                                                       m_mon_bfm_h.sample_miso_neg_11(miso);
+               write(m_mon_bfm_h.data_miso);
+                                                                      end
+                                                            endcase
+                                                      
+                                                        endtask : read_from_mon_bfm
+task  master_monitor_proxy::write(bit [DATA_LENGTH-1:0]data);
+
+  data_miso = data;
+    $display("WRITE__data_miso=%0d",data_miso);
+      data_mosi_q.push_front(data_miso);
+        ap.write(data_miso_q);
+        foreach(data_miso_q[i])
+        begin
+       $display(data_miso_q[i]);
+       end
+  
+endtask
+    
+    
+`endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 `endif
 
--- src/hvl_top/master/master_tx.sv
+++ src/hvl_top/master/master_tx.sv
@@ -1,40 +1,138 @@
 `ifndef MASTER_TX_INCLUDED_
 `define MASTER_TX_INCLUDED_
 
- //--------------------------------------------------------------------------------------------
- // Class: master_tx.
- // Description of the class.
- // This class holds the data items required to drive stimulus to dut
- // and also holds methods that manipulatethose data items
+//--------------------------------------------------------------------------------------------
+// Class: master_tx.
+//It's a transaction class that holds the SPI data items for generating the stimulus
  //--------------------------------------------------------------------------------------------
  class master_tx extends uvm_sequence_item;
-  
-  //register with factory so we can override with uvm method in future if necessary.
+    `uvm_object_utils(master_tx)
 
-  `uvm_object_utils(master_tx)
-
-  //-------------------------------------------------------
-  // Externally defined Tasks and Functions
-  //-------------------------------------------------------
+//-----------------------------------------------------
+// Instantiating SPI signals
+//------------------------------------------------------
+      bit sclk;
+      bit cs;
+      bit cpol;
+      bit cpha;
+                  
+      bit miso0;
+      bit miso1;
+      bit miso2;
+      bit miso3
+      
+      rand bit mosi0;
+      rand bit mosi1;
+      rand bit mosi2;
+      rand bit mosi3;
+    
+//------------------------------------------------------
+// Instantiation of Data Signals
+//------------------------------------------------------
+      bit [7:0] data_master_in_slave_out;
+      bit [7:0] data_master_out_slave_in;
+      
+//------------------------------------------------------
+// Externally defined Tasks and Functions
+//------------------------------------------------------
   extern function new(string name = "master_tx");
-  extern function void do_print(uvm_printer printer);
- endclass : master_tx
+  extern function void do_copy(uvm_object rhs);
+  extern function bit do_compare(uvm_object rhs, uvm_comparer comparer);
+  extern function void do_print(uvm_printer printer); 
+
+endclass : master_tx
 
 //--------------------------------------------------------------------------------------------
 // Construct: new
-// initializes the class object
+// construct the master_tx object
 //
 // Parameters:
 //  name - master_tx
 //--------------------------------------------------------------------------------------------
-  function master_tx::new(string name = "master_tx");
+function master_tx::new(string name = "master_tx");
     super.new(name);
-  endfunction : new
+endfunction : new
 
-  function void master_tx::do_print(uvm_printer printer);
-    super.do_print(printer);
-  endfunction : do_print
+//------------------------------------------------------
+// Function: do_copy
+// Copying transaction to another object
+//  
+// Parameters: 
+//  name - rhs
+//------------------------------------------------------
+function void master_tx::do_copy (uvm_object rhs);
   
+  // Variable: rhs_h
+  // New handle used to copy
+  master_tx rhs_h;
+           
+  if(!$cast(rhs_h,rhs)) begin
+   `uvm_fatal("do_copy","cast of the rhs object failed")
+  end
+  super.do_copy(rhs);
+  
+  cs    = rhs_h.cs;
+  miso0 = rhs_h.miso0;
+  mosi0 = rhs_h.mosi0;
+  
+endfunction : do_copy
 
+//-----------------------------------------------------
+//Function: do_compare
+//Comparing transaction from another object
+//
+//Parameters: 
+//name - rhs
+//----------------------------------------------------
+
+function bit master_tx::do_compare (uvm_object rhs, uvm_comparer comparer);
+  
+//Variable: rhs_h
+//New handle used to copy
+master_tx rhs_h;
+
+if(!$cast(rhs_h,rhs)) begin
+      `uvm_fatal("do_compare","cast of the rhs object failed")
+   return 0;
+ end
+
+return super.do_compare(rhs,comparer) &&
+cs    == rhs_h.cs &&
+mosi0 == rhs_h.mosi0 &&
+miso0 == rhs_h.miso0;
+
+endfunction:do_compare
+
+
+//------------------------------------------------------
+//Function: do_print
+//Comparing transaction from another object
+//
+//Parameters: 
+//name - printer
+//------------------------------------------------------
+function void slave_tx::do_print(uvm_printer printer);
+    super.do_print(printer);
+
+printer.print_field ("sclk",  sclk, 1, UVM_DEC);
+printer.print_field ("cs",    cs,   1, UVM_DEC);
+printer.print_field ("cpol",  cpol, 1, UVM_DEC);
+printer.print_field ("cpha",  cpha, 1, UVM_DEC);
+
+printer.print_field ("master_in_slave_out", data_master_in_slave_out, 8, UVM_HEX);
+printer.print_field ("master_out_slave_in", data_master_out_slave_in, 8, UVM_HEX);
+
+printer.print_field ("mosi0", mosi0, 1, UVM_DEC);
+printer.print_field ("mosi1", mosi1, 1, UVM_DEC);
+printer.print_field ("mosi2", mosi2, 1, UVM_DEC);
+printer.print_field ("mosi3", mosi3, 1, UVM_DEC);
+
+printer.print_field ("miso0", miso0, 1, UVM_DEC);
+printer.print_field ("miso1", miso1, 1, UVM_DEC);
+printer.print_field ("miso2", miso2, 1, UVM_DEC);
+printer.print_field ("miso3", miso3, 1, UVM_DEC);
+  
+endfunction : do_print
+  
 `endif
 
