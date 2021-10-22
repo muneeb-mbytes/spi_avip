0dad82333d29aeafd0d5a0686f24654a1fc1fcd8
Saketh Kokkanti <sakethkokkanti@mirafra.com>
2021-10-22 08:38:30 +0530

    i am adding master driver bfm file

--- src/hdl_top/master_agent_bfm/master_driver_bfm.sv
+++ src/hdl_top/master_agent_bfm/master_driver_bfm.sv
@@ -1,13 +1,133 @@
-//-------------------------------------------------------
-// master driver BFM
-//-------------------------------------------------------
+`ifndef MASTER_DRIVER_BFM_INCLUDED_
+`define MASTER_DRIVER_BFM_INCLUDED_
 
-module master_drv(spi_if intf);
+ //--------------------------------------------------------------------------------------------
+ // Interface:master_driver_bfm
+ // It connects with master_driver_proxy
+ //
+ // Parameters:
+ // intf - SPI Interface
+ //--------------------------------------------------------------------------------------------
+ interface master_driver_bfm (spi_if.MAS_DRV_MP m_drv_intf,spi_if.MON_MP mon_intf);
 
-virtual spi_if vif;
 
-initial 
-  begin
-    $display("Master driver BFM");
-  end
-endmodule
+   parameter int DATA_WIDTH =8;
+
+   //-------------------------------------------------------
+   // creating handle for proxy driver.
+   //-------------------------------------------------------
+   import spi_master_pkg::master_driver_proxy;
+   master_driver_proxy m_dry_proxy_h;
+
+
+   import spi_master_pkg::master_tx;
+   master_tx tx;
+  
+   initial begin
+   tx = new;
+   $display("Master Driver BFM");
+   end
+
+
+  
+  //--------------------------------------------------------------------------------------------
+  // Task for driving mosi signal and sampling miso signal for condition cpol==0,cpha==0
+  //--------------------------------------------------------------------------------------------
+  task drive_mosi_pos_miso_neg_cpol_0_cpha_0 (bit[7:0] data);
+    drive_msb_first_cpol_0_chpa_0(data);
+    drive_lsb_first_cpol_0_cpha_0(data);
+    drv_intf.SLV_DRV_MP.miso0 = tx.data_master_out_slave_in;
+  endtask : drive_mosi_pos_miso_neg_cpol_0_cpha_0
+  
+  // MSB is driven first
+  task drive_msb_first_cpol_0_cpha_0 (input bit[7:0] data);
+    for(int i=DATA_WIDTH; i>0 ; i--) begin
+      @(drv_intf.sample_miso_pos_cb.sclk);
+      drv_intf.SLV_DRV_MP.miso0 = data[i-1];
+    end
+  endtask : drive_msb_first_cpol_0_cpha_0
+  
+  // LSB is driven first
+  task drive_lsb_first_cpol_0_cpha_0 (input bit[7:0] data);
+    for(int i=0; i < DATA_WIDTH; i++) begin
+      @(drv_intf.sample_miso_pos_cb.sclk);
+      drv_intf.SLV_DRV_MP.miso0 = data[i];
+    end
+  endtask : drive_lsb_first_cpol_0_cpha_0
+  //--------------------------------------------------------------------------------------------
+  // Task for driving mosi signal and sampling miso signal for condition cpol==0,cpha==1
+  //--------------------------------------------------------------------------------------------
+  task drive_mosi_pos_miso_neg_cpol_0_cpha_1 (bit[7:0] data);
+    drive_msb_first_cpol_0_chpa_1(data);
+    drive_lsb_first_cpol_0_cpha_1(data);
+    drv_intf.SLV_DRV_MP.miso1 = tx.data_master_out_slave_in;
+  endtask : drive_mosi_pos_miso_neg_cpol_0_cpha_1
+  
+  // MSB is driven first
+  task drive_msb_first_cpol_0_cpha_1 (input bit[7:0] data);
+    for(int i=DATA_WIDTH; i>0 ; i--) begin
+      @(drv_intf.sample_miso_pos_cb.sclk);
+      drv_intf.SLV_DRV_MP.miso1 = data[i-1];
+    end
+  endtask : drive_msb_first_cpol_0_cpha_1
+  
+  // LSB is driven first
+  task drive_lsb_first_cpol_0_cpha_1 (input bit[7:0] data);
+    for(int i=0; i < DATA_WIDTH; i++) begin
+      @(drv_intf.sample_miso_pos_cb.sclk);
+      drv_intf.SLV_DRV_MP.miso1 = data[i];
+    end
+  endtask : drive_lsb_first_cpol_0_cpha_1
+  //--------------------------------------------------------------------------------------------
+  // Task for driving mosi signal and sampling miso signal for condition cpol==1,cpha==0
+  //--------------------------------------------------------------------------------------------
+  task drive_mosi_pos_miso_neg_cpol_1_cpha_0 (bit[7:0] data);
+    drive_msb_first_cpol_1_chpa_0(data);
+    drive_lsb_first_cpol_1_cpha_0(data);
+    drv_intf.SLV_DRV_MP.miso2 = tx.data_master_out_slave_in;
+  endtask : drive_mosi_pos_miso_neg_cpol_1_cpha_0
+  
+  // MSB is driven first
+  task drive_msb_first_cpol_1_cpha_0 (input bit[7:0] data);
+    for(int i=DATA_WIDTH; i>0 ; i--) begin
+      @(drv_intf.sample_miso_pos_cb.sclk);
+      drv_intf.SLV_DRV_MP.miso2 = data[i-1];
+    end
+  endtask : drive_msb_first_cpol_1_cpha_0
+  
+  // LSB is driven first
+  task drive_lsb_first_cpol_1_cpha_0 (input bit[7:0] data);
+    for(int i=0; i < DATA_WIDTH; i++) begin
+      @(drv_intf.sample_miso_pos_cb.sclk);
+      drv_intf.SLV_DRV_MP.miso2 = data[i];
+    end
+  endtask : drive_lsb_first_cpol_1_cpha_0
+
+  //--------------------------------------------------------------------------------------------
+  // Task for driving mosi signal and sampling miso signal for condition cpol==1,cpha==1
+  //--------------------------------------------------------------------------------------------
+  task drive_mosi_pos_miso_neg_cpol_1_cpha_1 (bit[7:0] data);
+    drive_msb_first_cpol_1_chpa_1(data);
+    drive_lsb_first_cpol_1_cpha_1(data);
+    drv_intf.SLV_DRV_MP.miso3 = tx.data_master_out_slave_in;
+  endtask : drive_mosi_pos_miso_neg_cpol_1_cpha_1
+  
+  // MSB is driven first
+  task drive_msb_first_cpol_1_cpha_1 (input bit[7:0] data);
+    for(int i=DATA_WIDTH; i>0 ; i--) begin
+      @(drv_intf.sample_miso_pos_cb.sclk);
+      drv_intf.SLV_DRV_MP.miso3 = data[i-1];
+    end
+  endtask : drive_msb_first_cpol_1_cpha_1
+  
+  // LSB is driven first
+  task drive_lsb_first_cpol_1_cpha_1 (input bit[7:0] data);
+    for(int i=0; i < DATA_WIDTH; i++) begin
+      @(drv_intf.sample_miso_pos_cb.sclk);
+      drv_intf.SLV_DRV_MP.miso3 = data[i];
+    end
+  endtask : drive_lsb_first_cpol_1_cpha_1
+
+ endinterface:master_driver_bfm
+
+`endif
