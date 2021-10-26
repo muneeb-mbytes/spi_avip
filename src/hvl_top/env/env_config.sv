436f38146167bda0e88e51a374503730cde8c9ee
Muneeb Ulla Shariff <muneebullashariff@gmail.com>
2021-10-26 13:41:45 +0530

    Update env_config.sv

--- src/hvl_top/env/env_config.sv
+++ src/hvl_top/env/env_config.sv
@@ -8,12 +8,6 @@
 class env_config extends uvm_object;
   `uvm_object_utils(env_config)
   
-    int no_of_magent = 1;
-    int no_of_sagent = 1;
-   
-    bit has_m_agt;
-    bit has_s_agt;
-  
   // Variable: has_scoreboard
   // Enables the scoreboard. Default value is 1
   bit has_scoreboard = 1;
@@ -28,11 +22,11 @@ class env_config extends uvm_object;
 
   // Variable: ma_cfg_h
   // Handle for master agent configuration
-    master_agent_config ma_cfg_h;
+  master_agent_config ma_cfg_h;
 
   // Variable: sa_cfg_h
   // Dynamic array of slave agnet configuration handles
-    slave_agent_config sa_cfg_h[];
+  slave_agent_config sa_cfg_h[];
 
   //-------------------------------------------------------
   // Externally defined Tasks and Functions
