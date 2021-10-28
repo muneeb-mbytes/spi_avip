41ec2983552413a9851c133c949062547c129cc6
Vamshidharreddymirafra <91044134+Vamshidharreddymirafra@users.noreply.github.com>
2021-10-28 18:38:07 +0530

    Update slave_agent_config.sv

--- src/hvl_top/slave/slave_agent_config.sv
+++ src/hvl_top/slave/slave_agent_config.sv
@@ -51,8 +51,8 @@ function void slave_agent_config::do_print(uvm_printer printer);
 //printer.print_field("is_active",is_active);
   printer.print_field ("is_active",is_active,1, UVM_DEC);
   printer.print_field ("slave_id",slave_id,2, UVM_DEC);
-  printer.print_field ("spi_mode",spi_mode, 2, UVM_HEX);
-  printer.print_field ("shift_dir",shift_dir, 1, UVM_DEC);
+  printer.print_field ("spi_mode",spi_mode, 2, UVM_ENUM);
+  printer.print_field ("shift_dir",shift_dir, 1, UVM_ENUM);
   
 endfunction:do_print
 
